39b792aa855e758c57aa69909f96bf39a5aefaa0
AndrySaprigin <coffeei.2002@gmail.com>
2022-02-08 16:41:37 +0300

    CopyFiles

---
+++ CoppingFiles/CoppingFiles/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace CoppingFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.From = new System.Windows.Forms.TextBox();
+            this.To = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BrowseFrom = new System.Windows.Forms.Button();
+            this.BrowseTo = new System.Windows.Forms.Button();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.OpneFile = new System.Windows.Forms.OpenFileDialog();
+            this.Copy = new System.Windows.Forms.Button();
+            this.OpenFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // From
+            // 
+            this.From.Location = new System.Drawing.Point(51, 19);
+            this.From.Name = "From";
+            this.From.Size = new System.Drawing.Size(240, 20);
+            this.From.TabIndex = 0;
+            // 
+            // To
+            // 
+            this.To.Location = new System.Drawing.Point(51, 50);
+            this.To.Name = "To";
+            this.To.Size = new System.Drawing.Size(240, 20);
+            this.To.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // BrowseFrom
+            // 
+            this.BrowseFrom.Location = new System.Drawing.Point(297, 17);
+            this.BrowseFrom.Name = "BrowseFrom";
+            this.BrowseFrom.Size = new System.Drawing.Size(80, 23);
+            this.BrowseFrom.TabIndex = 4;
+            this.BrowseFrom.Text = "Browse";
+            this.BrowseFrom.UseVisualStyleBackColor = true;
+            this.BrowseFrom.Click += new System.EventHandler(this.BrowseFrom_Click);
+            // 
+            // BrowseTo
+            // 
+            this.BrowseTo.Location = new System.Drawing.Point(297, 48);
+            this.BrowseTo.Name = "BrowseTo";
+            this.BrowseTo.Size = new System.Drawing.Size(80, 23);
+            this.BrowseTo.TabIndex = 5;
+            this.BrowseTo.Text = "Browse";
+            this.BrowseTo.UseVisualStyleBackColor = true;
+            this.BrowseTo.Click += new System.EventHandler(this.BrowseTo_Click);
+            // 
+            // Progress
+            // 
+            this.Progress.Location = new System.Drawing.Point(15, 87);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(276, 23);
+            this.Progress.TabIndex = 6;
+            // 
+            // OpneFile
+            // 
+            this.OpneFile.FileName = "openFileDialog1";
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(297, 87);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(80, 23);
+            this.Copy.TabIndex = 7;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 122);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.BrowseTo);
+            this.Controls.Add(this.BrowseFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.To);
+            this.Controls.Add(this.From);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox From;
+        private System.Windows.Forms.MaskedTextBox To;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BrowseFrom;
+        private System.Windows.Forms.Button BrowseTo;
+        private System.Windows.Forms.ProgressBar Progress;
+        private System.Windows.Forms.OpenFileDialog OpneFile;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.FolderBrowserDialog OpenFolder;
+    }
+}
+
---
+++ CoppingFiles/CoppingFiles/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoppingFiles
+{
+    public partial class Form1 : Form
+    {
+        private uint limit = 4096;
+        private string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFrom_Click(object sender, EventArgs e)
+        {
+            if (OpneFile.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = OpneFile.FileName;
+                fileName = Path.GetFileName(filePath);
+                int index = fileName.IndexOf('.');
+                fileName = fileName.Insert(index, "_Copy");
+                From.Text = filePath;
+            }
+        }
+
+        private void BrowseTo_Click(object sender, EventArgs e)
+        {
+            if (OpenFolder.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = OpenFolder.SelectedPath;
+                To.Text = filePath;
+            }
+        }
+
+        private byte[] Read()
+        {
+            return File.ReadAllBytes(From.Text);
+        }
+
+        private void WriteToFile(byte[] bytes)
+        {
+            string path =  To.Text + "\\" + fileName;
+
+            using (FileStream stream = File.Open(path, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    uint counter = 0;
+                    uint temp = 0;
+                    uint i = 0;
+
+                    while (counter < bytes.Length)
+                    {
+                        byte[] buffer = new byte[limit];
+
+                        for (i = 0; i < limit; i++)
+                        {
+                            if (i + counter == bytes.Length)
+                                break;
+
+                            buffer[i] = bytes[i + counter];
+                            temp++;
+                        }
+
+                        counter = temp;
+                        writer.Write(buffer);
+
+                    }
+                }
+            }
+        }
+
+        private async void CoppyFile(ProgressBar progressBar)
+        {
+            byte[] bytes = await Task.Run(() => Read());
+
+            //progressBar.Maximum = bytes.Length;
+            //progressBar.Step = bytes.Length / (int)limit;
+
+            if (bytes.Length <= 4096)
+                MessageBox.Show("File size less then 4096", "", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("File size more then 4096", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            await Task.Run(() => WriteToFile(bytes));
+
+            MessageBox.Show("File was copyed", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => CoppyFile(Progress));
+        }
+    }
+}
